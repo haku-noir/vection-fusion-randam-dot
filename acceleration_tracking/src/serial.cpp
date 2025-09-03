5bc2d22487b978eb29668ddf45c1c92baf179258
haku-noir <hakunoir.00@gmail.com>
2025-09-03 19:54:44 +0900

    M5Stackの開発環境をPlatformIOに変更

--- acceleration_tracking/src/serial.cpp
+++ acceleration_tracking/src/serial.cpp
@@ -10,7 +10,7 @@ const float G_TO_MS2 = 9.80665;
 void setup() {
   // M5Stackの初期化
   M5.begin();
-  
+
   // シリアル通信の開始（ボーレートはPython側と合わせる）
   Serial.begin(115200);
 
@@ -19,7 +19,7 @@ void setup() {
   // Basic/Gray -> MPU9250, Core2 -> MPU6886
   // Fire -> MPU9250
   // 下記の行はご自身のM5Stackに合わせて有効にしてください。
-  M5.IMU.Init(); 
+  M5.IMU.Init();
 
   M5.Lcd.setTextSize(2);
   M5.Lcd.println("XYZ Accel Sender");
@@ -33,12 +33,12 @@ void loop() {
   // 加速度センサのデータを取得 (単位: G)
   M5.IMU.getAccelData(&accX_g, &accY_g, &accZ_g);
 
-  // ★変更点: Gからm/s^2に単位を変換
+  // Gからm/s^2に単位を変換
   float accX_ms2 = accX_g * G_TO_MS2;
   float accY_ms2 = accY_g * G_TO_MS2;
   float accZ_ms2 = accZ_g * G_TO_MS2;
 
-  // ★変更点: m/s^2に変換したデータをカンマ区切りで送信
+  // m/s^2に変換したデータをカンマ区切りで送信
   Serial.printf("%.6f,%.6f,%.6f\n", accX_ms2, accY_ms2, accZ_ms2);
 
   // 画面にも値を表示（デバッグ用）
